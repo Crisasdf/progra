83ccdae180e3167b5298f5da2e112ab580170297
jornes99 <felipe.lacaussade123@gmail.com>
2021-02-04 01:26:39 -0300

    tarea 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
         System.setProperty("java.awt.headless", "false");
 
-        //Decir nombre y dar una caracteristica
+        /*Decir nombre y dar una caracteristica
         String name = JOptionPane.showInputDialog("Favor, diga su nombre: ");
         if ("rafa".equals(name) || "matias".equals(name) || "rafita".equals(name) || "guapo".equals(name)) {
             JOptionPane.showMessageDialog(null, "El " + name + " es flaco");
@@ -76,3 +76,65 @@ public class DemoApplication {
         }
     }
 }
+    */
+/*ejercicio 1
+        double cateto2 = Integer.parseInt(JOptionPane.showInputDialog(" indique su cateto2"));
+        double cateto3 = Integer.parseInt(JOptionPane.showInputDialog(" indique su cateto3"));
+
+            double hypo = 0;
+
+            hypo = (cateto2 * cateto2) + (cateto3 * cateto3);
+
+            hypo = Math.pow(hypo, 0.5);
+            {
+                JOptionPane.showMessageDialog(null, "La hipotenusa es " + " " + hypo);
+            }
+    }}*/
+  /*ejercicio 2
+
+        switch ( JOptionPane.showInputDialog("ingrese que opcion desea \n1) Saludo \n2) Despedida \n3) Agradecimiento \n4) Rabia")){
+            case "1":
+                //Saludo
+                JOptionPane.showMessageDialog(null,"Buens dias, como has estado");
+                break;
+            case "2":
+                // Despedida
+                JOptionPane.showMessageDialog(null,"Nos vemos, que estes bien");
+                break;
+            case "3":
+                //Agradecimiento
+                JOptionPane.showMessageDialog(null,"Gracias por su desempeño en esto");
+                break;
+            case "4":
+                //Rabia
+                JOptionPane.showMessageDialog(null,"Andai pasao a caca, vete a la verga");
+                break;
+            default:
+                //salir
+                JOptionPane.showMessageDialog(null," no tengo esos datos");
+        */
+
+        //Ejercicio 3
+       double tope = Double.parseDouble(JOptionPane.showInputDialog(" ingrese su numero"));
+        for (int i = 1; i <= tope; i++) {
+            double resultado = Math.pow(i, 0.5);
+         if (resultado % 1 == 0 )
+
+            JOptionPane.showMessageDialog(null, "El numero " + i + " Tiene raiz cuadrada");
+
+
+        }
+
+
+
+
+        /*Ejercicio3
+        El usuario ingresará un numero de tope y se deben mostrar las raices cuadradas ENTERAS de los numeros entre ese rango
+        Por ejemplo, si el usuario ingreso el numero 10, se deben calcular la raiz cuadrada de 1, 2, 3, 4, 5, 6, 7, 8, 9 y 10
+        pero solo se deben mostrar la raiz de 1, 4 y 9, ya que todas las otras raices contienen decimales, por lo tanto no son enteras.
+
+                Considerar
+                -Los inputs deben estar validados para que el programa no tenga errores de ejecución.
+                -La inversa un x² es √x (raiz cuadrada), por lo tanto, si sabemos que x² = 100, sacamos la raíz cuadrada del valor y nos queda x = 10*/
+
+    }}
